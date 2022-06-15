31dbd8cba2118b5417fbce67e12eb0b7e62ade76
Wamuyu <michellewachira25@gmail.com>
2022-06-16 01:51:24 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(CardsArr, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < CardsArr.length; i++) {
+        thankYouCards.push(`Thank you, ${CardsArr[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+
+function countDown(startNumber) {
+    while (startNumber > 0) {
+        console.log(startNumber);
+        startNumber -= 1;
+    }
+    console.log(startNumber);
+}
