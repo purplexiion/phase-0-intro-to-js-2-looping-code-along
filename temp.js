2cd4e87cc1ff20e9d27fb7a258ac3cf5b4934467
Wamuyu <michellewachira25@gmail.com>
2022-06-16 01:51:18 +0300

    commit tree e36a6e18409307f36d4b13cdf5df3beacc72704c

---
+++ temp.js
@@ -0,0 +1,28 @@
+// Code your solutions in this file
+
+for(let age =30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+ 
+}
+const gifts =["teddy bear","drone","doll"];
+function wrapGifts(gifts){
+    for(let i =0; 1 <gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    debugger;
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+writeCards=(["Wafula", "Michelle", "Oketch"], "birthday");
+
+function writingCards()
+{
+    for(i = 0;
+    i <= writeCards.length;
+   i++)
+  console.log("Dear ${writeCards}, have a happy ${writeCards} today!");
+ return writeCards;
+    }
